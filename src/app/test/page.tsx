b6c0f6bcb17cca583dd179c17b0bfcb91835fd0c
Samuel <91400290+sam-kmn@users.noreply.github.com>
2023-05-28 14:02:05 +0200

    clerk

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="container mx-auto h-[100dvh] flex justify-center items-center py-10">
-      <h1 className="text-3xl font-semibold">This is home page!</h1>
+      <h1 className="text-3xl font-semibold">This is test page!</h1>
     </div>
   )
 }
